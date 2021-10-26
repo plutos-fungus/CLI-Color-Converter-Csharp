78b37b259f605ed7fe4999109499efee30f60415
Plutos Fungus <rasmus1070@gmail.com>
2021-10-26 12:07:10 +0200

    Cleaned up the program so there is less code in the main program

---
+++ CLI-Color-Converter-Csharp/CLI-Color-Converter-Csharp/Converter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI_Color_Converter_Csharp
+{
+    public class Converter
+    {
+        /*============ Properties ============*/
+        public string HEX_R
+        { get; set; }
+        public string HEX_G
+        { get; set; }
+        public string HEX_B
+        { get; set; }
+        public string HEX
+        { get; set; }
+        public int R
+        { get; set; }
+        public int G
+        { get; set; }
+        public int B
+        { get; set; }
+
+        /*============ Methods ============*/
+        public int Converter_HEXRGB()
+        {
+            HEX = Console.ReadLine();
+            HEX_R = Convert.ToString(HEX[0]) + Convert.ToString(HEX[1]);
+            HEX_G = Convert.ToString(HEX[2]) + Convert.ToString(HEX[3]);
+            HEX_B = Convert.ToString(HEX[4]) + Convert.ToString(HEX[5]);
+            Console.WriteLine("HEX: " + HEX + " " + "HEX_R: " + HEX_R + " " + "HEX_G: " + HEX_G + " " + "HEX_B: " + HEX_B);
+            R = Convert.ToInt32(HEX_R, 16);
+            G = Convert.ToInt32(HEX_G, 16);
+            B = Convert.ToInt32(HEX_B, 16);
+            Console.WriteLine("R: " + R);
+            Console.WriteLine("G: " + G);
+            Console.WriteLine("B: " + B);
+            return 1;
+        }
+        public int Converter_RGBHEX()
+        {
+            R = Convert.ToInt32(Console.ReadLine());
+            G = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            HEX_R = Convert.ToString(R, 16);
+            HEX_G = Convert.ToString(G, 16);
+            HEX_B = Convert.ToString(B, 16);
+            HEX = HEX_R + HEX_G + HEX_B;
+            Console.WriteLine(HEX);
+            return 1;
+        }
+    }
+}
--- CLI-Color-Converter-Csharp/CLI-Color-Converter-Csharp/Program.cs
+++ CLI-Color-Converter-Csharp/CLI-Color-Converter-Csharp/Program.cs
@@ -7,44 +7,23 @@ namespace CLI_Color_Converter_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.   HEX --> RGB");
-            Console.WriteLine("2.   RGB --> HEX");
-
-            int R;
-            int G;
-            int B;
-            string HEX_R;
-            string HEX_G;
-            string HEX_B;
-            string HEX;
+            Console.WriteLine("======================");
+            Console.WriteLine("|| 1.   HEX --> RGB ||");
+            Console.WriteLine("|| 2.   RGB --> HEX ||");
+            Console.WriteLine("======================");
+            Converter Con = new Converter();
             int check = 0;
             check = Convert.ToInt32(Console.ReadLine());
             switch (check)
             {
                 case 1:
-                    HEX = Console.ReadLine();
-                    Console.WriteLine(HEX[0]);
-                    HEX_R = Convert.ToString(HEX[0]) + Convert.ToString(HEX[1]);
-                    HEX_G = Convert.ToString(HEX[2]) + Convert.ToString(HEX[3]);
-                    HEX_B = Convert.ToString(HEX[4]) + Convert.ToString(HEX[5]);
-                    Console.WriteLine("HEX: " + HEX + " " + "HEX_R: " + HEX_R + " " + "HEX_G: " + HEX_G  + " " + "HEX_B: " + HEX_B);
-                    R = Convert.ToInt32(HEX_R, 16);
-                    G = Convert.ToInt32(HEX_G, 16);
-                    B = Convert.ToInt32(HEX_B, 16);
-                    Console.WriteLine("R: " + R);
-                    Console.WriteLine("G: " + G);
-                    Console.WriteLine("B: " + B);
+                    Con.Converter_HEXRGB();
                     break;
                 case 2:
-                    R = Convert.ToInt32(Console.ReadLine());
-                    G = Convert.ToInt32(Console.ReadLine());
-                    B = Convert.ToInt32(Console.ReadLine());
-                    HEX_R = Convert.ToString(R, 16);
-                    HEX_G = Convert.ToString(G, 16);
-                    HEX_B = Convert.ToString(B, 16);
-                    HEX = HEX_R + HEX_G + HEX_B;
-                    Console.WriteLine(HEX);
-
+                    Con.Converter_RGBHEX();
+                    break;
+                default:
+                    Console.WriteLine("Please choose an option");
                     break;
             }
         }
